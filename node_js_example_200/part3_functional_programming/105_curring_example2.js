7f95ef183eae3ebbbc3597fab9fee00055f4cb61
conilon <conilon@naver.com>
2019-04-16 06:21:27 +0900

    PART3 - FUNCTIONAL PROGRAMMING

---
+++ node_js_example_200/part3_functional_programming/105_curring_example2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const openFileAndPrint = path => fileName => fs.readFile(path + fileName, (err, data) => {
+    console.log(path + fileName);
+    
+    if(err) throw err;
+    console.log(data.toString());
+});
+
+const thisDirOpenFileAndPrint = openFileAndPrint('./part3_functional_programming/');
+const otherDirOpenFileAndPrint = openFileAndPrint('../');
+
+thisDirOpenFileAndPrint('104_curring_example.js');
+otherDirOpenFileAndPrint('package.json');
